6bf4f33a1ceddd927e3efebacde6f2045c2a5177
rahul-iitd <Rahul.Bansal.cs116@cse.iitd.ac.in>
2020-01-27 21:06:05 +0530

    Initial Commit

---
+++ Campus Connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Campus Connect
+//
+//  Created by Rahul Bansal on 27/01/20.
+//  Copyright © 2020 Rahul Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
